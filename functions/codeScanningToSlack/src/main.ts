ac9931a93c410d26c69034c9eb09b975157026ed
NickLiffen <nickliffen@gmail.com>
2021-08-24 21:15:43 +0100

    pushing changes

--- functions/codeScanningToSlack/src/main.ts
+++ functions/codeScanningToSlack/src/main.ts
@@ -1,7 +1,7 @@
 import { ssm } from "./ssm";
 // import { IncomingWebhook } from '@slack/webhook'
 import { APIGatewayProxyEventV2, APIGatewayProxyResultV2 } from "aws-lambda";
-import { sign, verify } from "@octokit/webhooks-methods";
+import { verify } from "@octokit/webhooks-methods";
 
 export const handler = async (
   event: APIGatewayProxyEventV2
